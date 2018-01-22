4229f4f37f915155c368976dfe4feb0cb82299a2
JY <popjy0312@naver.com>
2018-01-22 09:18:20 -0800

    monitor mode pcap success

--- main.cpp
+++ main.cpp
@@ -1,9 +1,36 @@
-#include<stdio.h>
+#include <pcap.h>
+#include <stdio.h>
+#include <glog/logging.h>
 
 int main(int argc, char** argv){
+    char*                   dev;
+    pcap_t*                 handle;
+    char                    errbuf[PCAP_ERRBUF_SIZE];
+    const u_char*           packet;
+    struct pcap_pkthdr*     pheader;
+    uint32_t                res;
+
     if(argc != 2){
         printf("usage: airodump <interface>\n");
         return 1;
+    }
+    dev = argv[1];
+    if ((handle = pcap_create(dev, errbuf)) == NULL) {
+        fprintf(stderr, "Couldn't open device %s: %s\n", dev, errbuf);
+        return 1;
+    }
+    if(pcap_set_rfmon(handle, 1) == PCAP_ERROR_ACTIVATED){
+        fprintf(stderr, "set rfmon error\n");
+        return 1;
+    }
+    if(pcap_activate(handle) != 0){
+        fprintf(stderr, "Error activate handle %s", pcap_geterr(handle));
+        return 1;
+    }
+    while( (res = pcap_next_ex(handle, &pheader, &packet)) >= 0){
+        if (res == 0)
+            continue;
+        printf("len %d\n", pheader->len);
     }
     return 0;
 }
--- parse802.h
+++ parse802.h
@@ -2,10 +2,11 @@ struct radiotap{
     uint8_t h_revision;
     uint8_t h_pad;
     uint16_t h_len;
-    uint64_t pre_flag;
-    uint8_t flag;
+    uint32_t pre_flag;
 }__attribute__((__packed__));
 
 struct beacon{
 
 }__attribute__((__packed__));
+
+uint16_t getRadiotapHLen(uint8_t* p);
